3652966f1afc649a4bfd4ada543e3e033b5a5dd9
phinah <phinah.mahoro10@gmail.com>
2024-09-28 21:06:18 +0400

    feat: rain update

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
-"""calculates how much rain will be trapped after it rains
+"""
+Module to calculate how much rainwater is retained after it rains.
 """
 
 
 def rain(walls):
-    """calculates how much rain will be trapped after it rains
     """
-    if not walls or len(walls) < 3:
+    Calculate how many square units of water will be retained after it rains.
+
+    Arguments:
+    walls -- list of non-negative integers representing wall heights
+
+    Returns:
+    Integer indicating total amount of rainwater retained.
+    """
+    if not walls:
         return 0
 
-    rain = 0
-    for i in range(1, len(walls) - 1):
-        left = max(walls[:i])
-        right = max(walls[i + 1:])
-        min_wall = min(left, right)
-        if walls[i] < min_wall:
-            rain += min_wall - walls[i]
-    return rain
+    left, right = 0, len(walls) - 1
+    left_max, right_max = 0, 0
+    water = 0
+
+    while left < right:
+        if walls[left] <= walls[right]:
+            left_max = max(left_max, walls[left])
+            water += left_max - walls[left]
+            left += 1
+        else:
+            right_max = max(right_max, walls[right])
+            water += right_max - walls[right]
+            right -= 1
+
+    return water
